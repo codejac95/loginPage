9f72179ba1d498c7d7024c5a8ed9a8bc088eebfb
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-01-03 16:05:46 +0100

    fix #19

--- src/main/java/com/loginPage/loginPage/UserController.java
+++ src/main/java/com/loginPage/loginPage/UserController.java
@@ -1,18 +1,39 @@
 package com.loginPage.loginPage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@CrossOrigin
+
 public class UserController {
-    
+    @Autowired
     private UserRepository userRepository;
     
-    @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return userRepository.save(user);
+    
+   @GetMapping("/register")
+    public String registerUser() {
+        return "register";
     }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestParam("username")String newUsername,
+                               @RequestParam("password")String newPassword,
+                               @RequestParam("firstName")String newfirstName,
+                               @RequestParam("lastName")String newLastname,
+                               @RequestParam("mail")String newMail) {
+        User user = new User();
+        user.setUsername(newUsername);
+        user.setPassword(newPassword);
+        user.setFirstName(newfirstName);
+        user.setLastName(newLastname);
+        user.setMail(newMail);
+        userRepository.save(user);
+        return ("redirect:/");
+    }
+    
 }
