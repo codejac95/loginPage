7b91934ce486d4dd55a9d470c0b35a09cb587845
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-01-03 16:16:06 +0100

    hidden feature på password

--- src/main/java/com/loginPage/loginPage/UserController.java
+++ src/main/java/com/loginPage/loginPage/UserController.java
@@ -2,10 +2,8 @@ package com.loginPage.loginPage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
