22f2685b77f49ae39a65c762a95ee4ba00c82682
GTSA - Infinity <dante030724@gmail.com>
2024-01-05 10:13:27 +0100

    Roberts ändring

--- src/main/java/com/loginPage/loginPage/SecurityConfig.java
+++ src/main/java/com/loginPage/loginPage/SecurityConfig.java
@@ -1,13 +1,14 @@
-// package com.loginPage.loginPage;
+package com.loginPage.loginPage;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 // @Configuration
 // public class SecurityConfig {
-    
+
 //     @Bean
 //     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
@@ -16,3 +17,28 @@
 //         return http.build();
 //     }
 // }
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/order").authenticated()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/product/{productId}").permitAll()
+                        .requestMatchers("/register").permitAll()
+
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    public static void run(Class<SpringsecurityAplication> class1, String[] args) {
+    }
+
+}
---
+++ src/main/java/com/loginPage/loginPage/SpringsecurityAplication.java
@@ -0,0 +1,8 @@
+package com.loginPage.loginPage;
+
+public class SpringsecurityAplication {
+
+    public static void main(String[] args) {
+        SecurityConfig.run(SpringsecurityAplication.class, args);
+    }
+}
