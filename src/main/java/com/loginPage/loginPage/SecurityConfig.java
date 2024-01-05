9bbc5f176f9093103e8bc85a206cba3f47b74ea7
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-01-05 13:54:04 +0100

    massiva ändringar

--- src/main/java/com/loginPage/loginPage/SecurityConfig.java
+++ src/main/java/com/loginPage/loginPage/SecurityConfig.java
@@ -2,43 +2,25 @@ package com.loginPage.loginPage;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-// @Configuration
-// public class SecurityConfig {
-
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
-//         .anyRequest().authenticated()
-//         );
-//         return http.build();
-//     }
-// }
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeHttpRequests((request) -> request
-                        .requestMatchers("/order").authenticated()
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/product/{productId}").permitAll()
-                        .requestMatchers("/register").permitAll()
+        http.authorizeHttpRequests(auth -> auth  
+        .requestMatchers("/orders").authenticated()
+        .anyRequest().permitAll())
 
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-
+        
+        .formLogin(withDefaults());
+        
         return http.build();
-
-    }
-
-    public static void run(Class<SpringsecurityAplication> class1, String[] args) {
+        
     }
 }
 
